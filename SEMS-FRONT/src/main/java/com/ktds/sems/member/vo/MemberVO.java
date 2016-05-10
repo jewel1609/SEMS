51df6a13424e488d2aa68d685124cd83340fe875
sosdig <sosdig@a94d3de1-5e32-0510-9e34-86a7be50fea8>
2016-05-10 00:52:06 +0000

    memberVO 수정

--- SEMS-FRONT/src/main/java/com/ktds/sems/member/vo/MemberVO.java
+++ SEMS-FRONT/src/main/java/com/ktds/sems/member/vo/MemberVO.java
@@ -5,39 +5,39 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 public class MemberVO {
 
-	@NotEmpty(message="���̵�� �ʼ� �Է°� �Դϴ�.")
-	@Length(min=5, message="���̵�� 4�ڸ� ���ϸ� ���� �� �����ϴ�.")
+	@NotEmpty(message="아이디는 필수 입력 값 입니다!")
+	@Length(min=5, message="아이디는 영문, 숫자 조합의 4자리 이상 입력해야 합니다!")
 	private String id;
 	
-	@NotEmpty(message="��й�ȣ�� �ʼ� �Է°� �Դϴ�.")
-	@Length(min=10, message="Password�� 9�ڸ� ���ϸ� ���� �� �����ϴ�.")
+	@NotEmpty(message="비밀번호는 필수 입력 값 입니다!")
+	@Length(min=10, message="비밀번호는 영문, 숫자, 특수문자 조합의 10자리 이상 입력해야 합니다!")
 	private String password;
 	
-	@NotEmpty(message="�̸��� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="이름은 필수 입력 값 입니다!")
 	private String name;
 	
-	@NotEmpty(message="�̸��ϴ� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="이메일은 필수 입력 값 입니다!")
 	private String email;
 	
-	@NotEmpty(message="���� �з��� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="최종 학력는 필수 입력 값 입니다!")
 	private String highestEducationLevel;
 	
-	@NotEmpty(message="�б��� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="학교는 필수 입력 값 입니다!")
 	private String universityName;
 	
-	@NotEmpty(message="�а��� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="학과는 필수 입력 값 입니다!")
 	private String majorName;
 	
-	@NotEmpty(message="���� ���д� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="졸업 구분은 필수 입력 값 입니다!")
 	private String graduationType;
 	
-	@NotEmpty(message="��������� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="생년월일는 필수 입력 값 입니다!")
 	private String birthDate;
 	
-	@NotEmpty(message="��ȭ��ȣ�� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="전화 번호는 필수 입력 값 입니다!")
 	private String phoneNumber;
 	
-	@NotEmpty(message="��й�ȣ�� �ʼ� �Է°� �Դϴ�.")
+	@NotEmpty(message="회원 종류는 필수 입력 값 입니다!")
 	private String memberType;
 
 	private String salt;
@@ -90,10 +90,10 @@ public class MemberVO {
 	public void setGraduationType(String graduationType) {
 		this.graduationType = graduationType;
 	}
-	public String getBrithDate() {
+	public String getBirthDate() {
 		return birthDate;
 	}
-	public void setBrithDate(String birthDate) {
+	public void setBirthDate(String birthDate) {
 		this.birthDate = birthDate;
 	}
 	public String getPhoneNumber() {
