bca52ccf9a85a727fabdf67af11edb4b177b99ff
hihelloho <hihelloho@a94d3de1-5e32-0510-9e34-86a7be50fea8>
2016-05-11 07:30:13 +0000

    interceptor

---
+++ SEMS-FRONT/src/main/java/com/ktds/sems/interceptor/FirstInterceptor.java
@@ -0,0 +1,27 @@
+package com.ktds.sems.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.ktds.sems.common.Session;
+
+public class FirstInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		
+		// 페이지 접근할 때, 세션이 있는지 없는지 체크
+		boolean wasLogin = request.getSession().getAttribute(Session.MEMBER) != null;
+		
+		if ( wasLogin ) {
+			System.out.println("세션 있음");
+			response.sendRedirect("/main");
+		} else {
+			System.out.println("세션 없음");
+		}
+		
+		return true;
+	}
+}
