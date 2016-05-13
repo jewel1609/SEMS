6a8ca9989659c6109113192d8b09354d031a348b
gipyo1004 <gipyo1004@a94d3de1-5e32-0510-9e34-86a7be50fea8>
2016-05-13 02:07:38 +0000

    EducationControllerTest

---
+++ SEMS-COMM/src/test/java/com/ktds/sems/education/web/EducationControllerTest.java
@@ -0,0 +1,118 @@
+package com.ktds.sems.education.web;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ktds.sems.education.vo.CategoryVO;
+import com.ktds.sems.education.vo.JsonResponseVO;
+import com.ktds.sems.education.vo.CostVO;
+import com.ktds.sems.education.vo.TimeVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+								  "/applicationContext.xml"
+								, "/educationContext.xml"
+								, "/memberContext.xml"
+								, "/menuContext.xml"
+								, "/rootContext.xml"})
+public class EducationControllerTest {
+
+	@Autowired
+	private EducationController educationController;
+	
+	@Test
+	public void viewCategoryPageTest(){
+		ModelAndView view = educationController.viewCategoryPage();
+		String viewName = view.getViewName();
+		assertNotNull(viewName);
+		assertTrue(viewName == "education/category");
+	}
+	
+	@Test
+	public void viewCostPageTest() {
+		ModelAndView view = educationController.viewCostPage();
+		assertNotNull(view);
+		
+		if ( view != null ) {
+			String viewName = view.getViewName();
+			assertNotNull(viewName);
+			assertEquals(viewName, "education/cost");
+			
+			List<CostVO> costList = (List<CostVO>) view.getModelMap().get("costList");
+			assertNotNull(costList);
+			assertTrue(costList.size() > 0);
+		}
+		else {
+			fail("view is null");
+		}
+	}
+	
+	@Test
+	public void deleteEduCostTest() {
+		String cdId = "CSTF";
+		
+		ModelAndView view = educationController.deleteEduCost(cdId);
+		assertNotNull(view);
+		
+		if ( view != null ) {
+			String viewName = view.getViewName();
+			assertNotNull(viewName);
+			assertEquals(viewName, "redirect:/education/cost");
+		}
+		else {
+			fail("view is null");
+		}
+	}
+	
+	@Test
+	public void viewTimePageTest() {
+		ModelAndView view = educationController.viewTimePage();
+		assertNotNull(view);
+		
+		if ( view != null ) {
+			String viewName = view.getViewName();
+			assertNotNull(viewName);
+			assertEquals(viewName, "education/time");
+			
+			List<TimeVO> timeList = (List<TimeVO>) view.getModelMap().get("timeList");
+			assertNotNull(timeList);
+			assertTrue(timeList.size() > 0);
+		}
+		else {
+			fail("view is null");
+		}
+	}
+	
+	@Test
+	public void deleteEduTimeTest() {
+		String cdId = "TIMD";
+		
+		ModelAndView view = educationController.deleteEduTime(cdId);
+		assertNotNull(view);
+		
+		if ( view != null ) {
+			String viewName = view.getViewName();
+			assertNotNull(viewName);
+			assertEquals(viewName, "redirect:/education/time");
+		}
+		else {
+			fail("view is null");
+		}
+	}
+
+}
