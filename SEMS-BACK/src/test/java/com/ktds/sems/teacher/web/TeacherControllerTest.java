37c6efbccc400ada603cdb45c6c071bba4128614
showil20 <showil20@a94d3de1-5e32-0510-9e34-86a7be50fea8>
2016-05-19 00:34:12 +0000

    teacherController

--- SEMS-BACK/src/test/java/com/ktds/sems/teacher/web/TeacherControllerTest.java
+++ SEMS-BACK/src/test/java/com/ktds/sems/teacher/web/TeacherControllerTest.java
@@ -3,34 +3,29 @@ package com.ktds.sems.teacher.web;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
-
-import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.ktds.sems.SemsTestCase;
 import com.ktds.sems.common.Session;
-import com.ktds.sems.teacher.vo.EducationHistoryVO;
-import com.ktds.sems.teacher.vo.ProjectHistoryVO;
 import com.ktds.sems.teacher.vo.TeacherBookVO;
 import com.ktds.sems.teacher.vo.TeacherVO;
+import com.ktds.sems.teacher.vo.EducationHistoryVO;
+import com.ktds.sems.teacher.vo.ProjectHistoryVO;
 import com.ktds.sems.validator.teacher.TeacherVOValidator;
 
-public class TeacherControllerTest extends SemsTestCase{
+public class TeacherControllerTest extends SemsTestCase {
 
 	@Autowired
 	private TeacherController teacherController;
-	
+
 	@Test
-	public void viewDetailPageTest(){
+	public void viewDetailPageTest() {
 		String memberId = "teacher02";
 		ModelAndView view = teacherController.viewDetailPage(memberId);
 		assertNotNull(view);
@@ -46,6 +41,210 @@ public class TeacherControllerTest extends SemsTestCase{
 		
 		teacherVO.setAnnual(1);
 		teacherVO.setMemberId("teacher02");
+		
+		List<TeacherBookVO> teacherBookList = new ArrayList<TeacherBookVO>();
+		TeacherBookVO teacherBookVO = new TeacherBookVO();
+		teacherBookVO.setId("AR-20160516-000001");
+		teacherBookList.add(teacherBookVO);
+		teacherVO.setTeacherBookList(teacherBookList);
+		
+		List<ProjectHistoryVO> projectHistoryList = new ArrayList<ProjectHistoryVO>();
+		ProjectHistoryVO projectHistoryVO = new ProjectHistoryVO();
+		projectHistoryVO.setId("AR-20160516-000002");
+		projectHistoryList.add(projectHistoryVO);
+		teacherVO.setProjectHistoryList(projectHistoryList);
+		
+		List<EducationHistoryVO> educationHistoryList = new ArrayList<EducationHistoryVO>();
+		EducationHistoryVO educationHistoryVO = new EducationHistoryVO();
+		educationHistoryVO.setId("AR-20160516-000001");
+		educationHistoryList.add(educationHistoryVO);
+		teacherVO.setEducationHistoryList(educationHistoryList);
+		
+		// 2. Errors
+		BindingResult errors = new BeanPropertyBindingResult(teacherVO, "teacherUpdateForm");
+		TeacherVOValidator validator = new TeacherVOValidator();
+		validator.validate(teacherVO, errors);
+		
+		// 3. Session
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute(Session.MEMBER_TYPE, "ADM");
+		
+		// 파라미터 3개 확인
+		ModelAndView view = teacherController.doTeacherInfoModifyAction(teacherVO, errors, session);
+		
+		if(view != null) {
+			
+			String viewName = view.getViewName();
+			assertNotNull(viewName);
+			
+		} else {
+			fail("fail");
+		}
+	}
+	@Test
+	public void doTeacherInfoModifyActionTestError1(){
+		
+		// 1. TeacherVO
+		TeacherVO teacherVO = new TeacherVO();
+		
+		teacherVO.setAnnual(1); 
+		teacherVO.setMemberId("teacher02");
+		teacherVO.setBusinessNumber("JUNIT...TT^TT");
+		teacherVO.setCompanyName("JUNIT...TT^TT");
+		teacherVO.setName("JUNIT...TT^TT");
+		
+		List<TeacherBookVO> teacherBookList = new ArrayList<TeacherBookVO>();
+		TeacherBookVO teacherBookVO = new TeacherBookVO();
+		teacherBookVO.setId("AR-20160516-000001");
+		teacherBookList.add(teacherBookVO);
+		teacherVO.setTeacherBookList(teacherBookList);
+		
+		List<ProjectHistoryVO> projectHistoryList = new ArrayList<ProjectHistoryVO>();
+		ProjectHistoryVO projectHistoryVO = new ProjectHistoryVO();
+		projectHistoryVO.setId("AR-20160516-000002");
+		projectHistoryList.add(projectHistoryVO);
+		teacherVO.setProjectHistoryList(projectHistoryList);
+		
+		List<EducationHistoryVO> educationHistoryList = new ArrayList<EducationHistoryVO>();
+		EducationHistoryVO educationHistoryVO = new EducationHistoryVO();
+		educationHistoryVO.setId("AR-20160516-000001");
+		educationHistoryList.add(educationHistoryVO);
+		teacherVO.setEducationHistoryList(educationHistoryList);
+		
+		// 2. Errors
+		BindingResult errors = new BeanPropertyBindingResult(teacherVO, "teacherUpdateForm");
+		TeacherVOValidator validator = new TeacherVOValidator();
+		validator.validate(teacherVO, errors);
+		
+		// 3. Session
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute(Session.MEMBER_TYPE, "ADM");
+		
+		// 파라미터 3개 확인
+		ModelAndView view = teacherController.doTeacherInfoModifyAction(teacherVO, errors, session);
+		
+		if(view != null) {
+			
+			String viewName = view.getViewName();
+			assertNotNull(viewName);
+			
+		} else {
+			fail("fail");
+		}
+	}
+	@Test
+	public void doTeacherInfoModifyActionTestError2(){
+		
+		// 1. TeacherVO
+		TeacherVO teacherVO = new TeacherVO();
+		
+		teacherVO.setAnnual(1);
+		teacherVO.setMemberId("teacher02");
+//		teacherVO.setBusinessNumber("JUNIT...TT^TT");
+		teacherVO.setCompanyName("JUNIT...TT^TT");
+		teacherVO.setName("JUNIT...TT^TT");
+		
+		List<TeacherBookVO> teacherBookList = new ArrayList<TeacherBookVO>();
+		TeacherBookVO teacherBookVO = new TeacherBookVO();
+		teacherBookVO.setId("AR-20160516-000001");
+		teacherBookList.add(teacherBookVO);
+		teacherVO.setTeacherBookList(teacherBookList);
+		
+		List<ProjectHistoryVO> projectHistoryList = new ArrayList<ProjectHistoryVO>();
+		ProjectHistoryVO projectHistoryVO = new ProjectHistoryVO();
+		projectHistoryVO.setId("AR-20160516-000002");
+		projectHistoryList.add(projectHistoryVO);
+		teacherVO.setProjectHistoryList(projectHistoryList);
+		
+		List<EducationHistoryVO> educationHistoryList = new ArrayList<EducationHistoryVO>();
+		EducationHistoryVO educationHistoryVO = new EducationHistoryVO();
+		educationHistoryVO.setId("AR-20160516-000001");
+		educationHistoryList.add(educationHistoryVO);
+		teacherVO.setEducationHistoryList(educationHistoryList);
+		
+		// 2. Errors
+		BindingResult errors = new BeanPropertyBindingResult(teacherVO, "teacherUpdateForm");
+		TeacherVOValidator validator = new TeacherVOValidator();
+		validator.validate(teacherVO, errors);
+		
+		// 3. Session
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute(Session.MEMBER_TYPE, "ADM");
+		
+		// 파라미터 3개 확인
+		ModelAndView view = teacherController.doTeacherInfoModifyAction(teacherVO, errors, session);
+		
+		if(view != null) {
+			
+			String viewName = view.getViewName();
+			assertNotNull(viewName);
+			
+		} else {
+			fail("fail");
+		}
+	}
+	@Test
+	public void doTeacherInfoModifyActionTestError3(){
+		
+		// 1. TeacherVO
+		TeacherVO teacherVO = new TeacherVO();
+		
+		teacherVO.setAnnual(1);
+		teacherVO.setMemberId("teacher02");
+		teacherVO.setBusinessNumber("JUNIT...TT^TT");
+//		teacherVO.setCompanyName("JUNIT...TT^TT");
+		teacherVO.setName("JUNIT...TT^TT");
+		
+		List<TeacherBookVO> teacherBookList = new ArrayList<TeacherBookVO>();
+		TeacherBookVO teacherBookVO = new TeacherBookVO();
+		teacherBookVO.setId("AR-20160516-000001");
+		teacherBookList.add(teacherBookVO);
+		teacherVO.setTeacherBookList(teacherBookList);
+		
+		List<ProjectHistoryVO> projectHistoryList = new ArrayList<ProjectHistoryVO>();
+		ProjectHistoryVO projectHistoryVO = new ProjectHistoryVO();
+		projectHistoryVO.setId("AR-20160516-000002");
+		projectHistoryList.add(projectHistoryVO);
+		teacherVO.setProjectHistoryList(projectHistoryList);
+		
+		List<EducationHistoryVO> educationHistoryList = new ArrayList<EducationHistoryVO>();
+		EducationHistoryVO educationHistoryVO = new EducationHistoryVO();
+		educationHistoryVO.setId("AR-20160516-000001");
+		educationHistoryList.add(educationHistoryVO);
+		teacherVO.setEducationHistoryList(educationHistoryList);
+		
+		// 2. Errors
+		BindingResult errors = new BeanPropertyBindingResult(teacherVO, "teacherUpdateForm");
+		TeacherVOValidator validator = new TeacherVOValidator();
+		validator.validate(teacherVO, errors);
+		
+		// 3. Session
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute(Session.MEMBER_TYPE, "ADM");
+		
+		// 파라미터 3개 확인
+		ModelAndView view = teacherController.doTeacherInfoModifyAction(teacherVO, errors, session);
+		
+		if(view != null) {
+			
+			String viewName = view.getViewName();
+			assertNotNull(viewName);
+			
+		} else {
+			fail("fail");
+		}
+	}
+	@Test
+	public void doTeacherInfoModifyActionTestError4(){
+		
+		// 1. TeacherVO
+		TeacherVO teacherVO = new TeacherVO();
+		
+		teacherVO.setAnnual(1);
+		teacherVO.setMemberId("teacher02");
+		teacherVO.setBusinessNumber("JUNIT...TT^TT");
+		teacherVO.setCompanyName("JUNIT...TT^TT");
+//		teacherVO.setName("JUNIT...TT^TT");
 		
 		List<TeacherBookVO> teacherBookList = new ArrayList<TeacherBookVO>();
 		TeacherBookVO teacherBookVO = new TeacherBookVO();
