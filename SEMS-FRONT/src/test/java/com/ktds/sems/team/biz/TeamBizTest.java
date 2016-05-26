fa6fd9c043694ddafbf358e6288a594e1bc1c7ee
showil20 <showil20@a94d3de1-5e32-0510-9e34-86a7be50fea8>
2016-05-26 05:04:49 +0000

    테스트 커밋

--- SEMS-FRONT/src/test/java/com/ktds/sems/team/biz/TeamBizTest.java
+++ SEMS-FRONT/src/test/java/com/ktds/sems/team/biz/TeamBizTest.java
@@ -1,9 +1,16 @@
 package com.ktds.sems.team.biz;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ktds.sems.SemsTestCase;
+import com.ktds.sems.team.vo.TeamSearchVO;
+import com.ktds.sems.team.vo.TeamVO;
 
 @Transactional
 public class TeamBizTest extends SemsTestCase {
@@ -11,6 +18,23 @@ public class TeamBizTest extends SemsTestCase {
 	@Autowired
 	private TeamBiz teamBiz;
 
-	
-	
+	@Test
+	public void getAllTeamListTest() {
+		TeamSearchVO searchVO = new TeamSearchVO();
+		
+		searchVO.setSearchKeyword("");
+		searchVO.setSearchType("");
+		searchVO.setStartIndex(0);
+		searchVO.setEndIndex(10);
+		
+		List<TeamVO> teamList = teamBiz.getAllTeamList(searchVO);
+		assertNotNull(teamList);
+		if (teamList != null){
+			assertTrue(teamList.size() >0);
+		}
+		else{
+			fail("Fail...");
+		}
+	}
 }
+	
